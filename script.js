535c780a9495e71a687654f4fdef59b9f4f87944
CJB92324 <26brenicad@casdonline.org>
2025-09-01 21:27:14 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -1,73 +1,54 @@
-// const colors = ['Red', 'Green', 'Blue', 'Brown', 'Purple'];
-
-const words = ['red', 'blue', 'purple', 'green', 'brown', 'red', 'green', 'purple', 'blue', 'brown', 'end']
-//const colors = ['Green', 'Brown', 'Red', 'Blue', 'Purple', 'Blue', 'Brown', 'Green', 'Red', 'Purple', 'Black']
-const colors = ['Black', 'Black', 'Black', 'Black', 'Black', 'Black', 'Black', 'Black', 'Black', 'Black', 'Black']
+const words = ['red', 'blue', 'purple', 'green', 'brown', 'red', 'green', 'purple', 'blue', 'brown', 'end', '']
+const colors = ['Black', 'Black', 'Black', 'Black', 'Black', 'Black', 'Black', 'Black', 'Black', 'Black', 'Black', '']
 const ccjWord = document.getElementById('ccj-word');
 const infoScreen = document.getElementById('info-screen');
-const readyScreen = document.getElementById('ready-screen');
-//const (word, color);
+const infoScreen2 = document.getElementById('info-screen-2');
 
-let currentState = 'info';
-//let nextState = 'stroop';
-let currentWord = 0;
-let currentPair = ['black', 'Black'];
-
-/* function randomColorWord() {
-	const word = colors[Math.floor(Math.random()*colors.length)];
-	
-	let color;
-	do {
-		color = colors[Math.floor(Math.random() * colors.length)];
-	} while (color == word);
-	
-return {word, color};
-}
-*/
-function nextWord(stage) {
-	currentPair[0] = words[stage];
-	currentPair[1] = colors[stage];
-}
-
-function setScreen(state) {
-		infoScreen.style.display = 'none';
-		readyScreen.style.display = 'none';
-		ccjWord.style.display = 'none';
+let info = false;
+let stroop = false;
+function stroopTest() {
+	stroop = true;
+	infoScreen.style.display = 'none';
+	infoScreen2.style.display = 'none';
+	ccjWord.textContent = words[0];
+	ccjWord.style.color = colors[0].toLowerCase();
+	ccjWord.style.display = 'block';
+	let count = 1;
+	const maxIts = 12;
+	const intTime = 2000;
+	const intId = setInterval(() => {
+		ccjWord.textContent = words[count];
+		ccjWord.style.color = colors[count].toLowerCase();
+		ccjWord.style.display = 'block';
 		
-		if (state === 'info') {
+		count++;
+		
+		
+		if (count >= maxIts) {
+			clearInterval(intId);
 			infoScreen.style.display = 'block';
-		} else if (state === 'ready') {
-			readyScreen.style.display = 'block';
-		} 
-		//else if (currentWord == words.length) {
-		//	break;
-		//} 
-		else if (state === 'stroop') {
-			nextWord(currentWord);
-			ccjWord.textContent = currentPair[0];
-			ccjWord.style.color = currentPair[1].toLowerCase();
-			currentWord++;
-			ccjWord.style.display = 'block';
+			infoScreen2.style.display = 'none';
+			ccjWord.style.display = 'none';
+			info = false;
+			stroop = false;
 		}
-		
-		currentState = state;
+	}, intTime);
 }
 
 document.addEventListener('keydown', (event) => {
+	if (stroop != true) {
 	if (event.code === 'Space') {
 		event.preventDefault();
 		
-		if (currentState === 'info') {
-			setScreen('stroop');
-		} else if (currentState === 'stroop') {
-			if (currentWord === words.length) {
-				currentWord = 0;
-				setScreen('info');
-			} else {
-				setScreen('ready');
-			}
-		} else if (currentState === 'ready') {
-			setScreen('stroop');
+		if (info === false) {
+			infoScreen.style.display = 'none';
+			infoScreen2.style.display = 'block';
+			console.log("flag found");
+			info = true;
+		} else {
+		console.log("stroop started");
+		stroopTest();
 		}
+	}
 	}
 });
